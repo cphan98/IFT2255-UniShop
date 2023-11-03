57b8e13b49f1331f49dddaf8f684a68b8dd85b8c
Cynthia Phan <cynthiaphan.code@gmail.com>
2023-11-03 19:54:41 -0400

    implement CreditCard class

---
+++ implementation/UniShop/src/main/java/Users/CreditCard.java
@@ -0,0 +1,23 @@
+package Users;
+
+import java.util.Date;
+
+public class CreditCard {
+    // attributes
+    private int cardNumber;
+    private String firstName;
+    private String lastName;
+    private Date expiryDate;
+
+    // getters
+    public int getCardNumber() { return cardNumber; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public Date getExpiryDate() { return expiryDate; }
+
+    // setters
+    public void setCardNumber(int cardNumber) { this.cardNumber = cardNumber; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setExpiryDate(Date expiaryDate) { this.expiryDate = expiaryDate; }
+}
