29c3cb002dc9cf466a81056359a9bec26bf8ff5a
Cynthia Phan <cynthiaphan.code@gmail.com>
2023-12-15 22:07:50 -0500

    create unit tests for addExpPoints, addBuyPoints and addProduct

---
+++ implementation/UniShop/src/test/java/BuyerMetricsTest.java
@@ -0,0 +1,56 @@
+import BackEndUtility.*;
+import Metrics.*;
+import productClasses.*;
+import serializationUtil.*;
+import UIs.*;
+import Users.*;
+import UtilityObjects.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BuyerMetricsTest {
+
+    private DataBase database;
+    private Buyer buyer;
+
+    @BeforeEach
+    void setUp() {
+        // create buyer
+        Address address = new Address("17 Doctor Dr.", "Canada", "Grand Line", "Drum Island",
+                "C0P1P1");
+        buyer = new Buyer("Tony Tony", "Chopper", "dr_chopper", "cottoncandy",
+                "dr_chopper@onepice.com", "0123456789", address);
+
+        // create list of users
+        ArrayList<User> users = new ArrayList<>();
+        users.add(buyer);
+
+        // initialize database with list of users
+        database = new DataBase(users);
+    }
+
+    @Test
+    void testAddExpPoints() {
+        // add experience points to buyer
+        int xp = 1999;
+        database.getBuyer(buyer).getMetrics().addExpPoints(xp);
+
+        // assert addExpPoints
+        assertEquals(xp, database.getBuyer(buyer).getMetrics().getExpPoints());
+    }
+
+    @Test
+    void testAddBuyPoints() {
+        // add buy points to buyer
+        int points = 2023;
+        database.getBuyer(buyer).getMetrics().addBuyPoints(points);
+
+        // assert addBuyPoints
+        assertEquals(points, database.getBuyer(buyer).getMetrics().getBuyPoints());
+    }
+}
