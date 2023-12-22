fbbc3cd0d3283e496a95687fc44dddb1ea67acee
laura <lauracadillo13@gmail.com>
2023-12-21 21:47:39 -0500

    javadoc for some classes

--- implementation/UniShop/src/main/java/Metrics/Metrics.java
+++ implementation/UniShop/src/main/java/Metrics/Metrics.java
@@ -1,10 +1,16 @@
 package Metrics;
-
+/**
+ * The Metrics interface provides utility methods for handling metrics-related operations.
+ */
 public interface Metrics {
 
     // UTILITIES
 
-    // to string
+    /**
+     * Returns a string representation of the metrics.
+     *
+     * @return A string representing the metrics.
+     */
 
     public String toString();
 }
--- implementation/UniShop/src/main/java/Metrics/SellerMetrics.java
+++ implementation/UniShop/src/main/java/Metrics/SellerMetrics.java
@@ -3,7 +3,11 @@ package Metrics;
 import BackEndUtility.InputManager;
 
 import java.util.ArrayList;
-
+/**
+ * The SellerMetrics class represents the metrics of a seller in UniShop.
+ * It tracks various metrics such as revenue, products sold, average note received, and likes.
+ *
+ */
 public class SellerMetrics implements Metrics, java.io.Serializable {
 
     // ATTRIBUTES
@@ -16,7 +20,9 @@ public class SellerMetrics implements Metrics, java.io.Serializable {
     public ArrayList<String> selectedMetrics;//to be shown on the users profile
 
     // CONSTRUCTOR
-
+    /**
+     * Constructs a SellerMetrics object with initial metric values for the seller.
+     */
     public SellerMetrics() {
         revenue = 0;
         productsSold = 0;
@@ -26,47 +32,83 @@ public class SellerMetrics implements Metrics, java.io.Serializable {
     }
 
     // GETTERS
-
+    /**
+     * Retrieves the list of selected metrics to display in the profile.
+     *
+     * @return The list of selected metrics by the user.
+     */
     public ArrayList<String> getSelectedMetrics(){
         return selectedMetrics;
     }
-
+    /**
+     * Retrieves the revenue for the seller.
+     *
+     * @return The revenue metric.
+     */
     public float getRevenue() {
         return revenue;
     }
-
+    /**
+     * Retrieves number of the products sold by the seller.
+     *
+     * @return The products sold metric.
+     */
     public int getProductsSold() {
         return productsSold;
     }
-
+    /**
+     * Retrieves the average note received by the seller.
+     *
+     * @return The average note received metric.
+     */
     public float getAverageNoteReceived() {
         return averageNoteReceived;
     }
-
+    /**
+     * Retrieves the likes that the seller has obtained.
+     *
+     * @return The likes metric.
+     */
     public int getLikes() {
         return likes;
     }
 
     // SETTERS
-
+    /**
+     * Sets the list of selected metrics.
+     *
+     * @param selectedMetrics The new list of selected metrics.
+     */
     public void setSelectedMetrics(ArrayList<String> selectedMetrics) {this.selectedMetrics = selectedMetrics; };
 
     // UTILITIES
 
     // revenue --------------------------------------------------------------------------------------------------------
-
+    /**
+     * Updates the revenue metric.
+     *
+     * @param revenue The new revenue value for the seller.
+     */
     public void updateRevenue(float revenue) {
         this.revenue = revenue;
     }
 
     // products -------------------------------------------------------------------------------------------------------
-
+    /**
+     * Updates the number of products sold metric.
+     *
+     * @param productsSold The new value for products sold.
+     */
     public void updateProductsSold(int productsSold) {
         this.productsSold = productsSold;
     }
 
     // evaluations ----------------------------------------------------------------------------------------------------
-
+    /**
+     * Updates the average note received metric.
+     *
+     * @param note The new note value to be added to the notesReceived list.
+     */
     public void updateAverageNoteReceived(float note) {
         notesReceived.add(note);
         float sum = 0;
@@ -75,7 +117,11 @@ public class SellerMetrics implements Metrics, java.io.Serializable {
         }
         averageNoteReceived = Math.round((sum / notesReceived.size()) * 10) / 10f;
     }
-
+    /**
+     * Removes a note from the notesReceived list and updates the average note received metric.
+     *
+     * @param note The note value to be removed from the notesReceived list.
+     */
     public void removeAverageNoteReceived(float note) {
         notesReceived.remove(note);
         float sum = 0;
@@ -86,13 +132,19 @@ public class SellerMetrics implements Metrics, java.io.Serializable {
     }
 
     // likes ----------------------------------------------------------------------------------------------------------
-
+    /**
+     * Updates the likes metric.
+     *
+     * @param likes The new value for likes.
+     */
     public void updateLikes(int likes) {
         this.likes = likes;
     }
 
     // configuration --------------------------------------------------------------------------------------------------
-
+    /**
+     * Configures the metrics, allowing the seller to select metrics to be displayed on their profile.
+     */
     public void configureMetrics(){
         ArrayList<String> allMetrics = new ArrayList<>();
         ArrayList<Number> allMetricsValues =new ArrayList<>();
@@ -164,12 +216,20 @@ public class SellerMetrics implements Metrics, java.io.Serializable {
     }
 
     // to string ------------------------------------------------------------------------------------------------------
-
+    /**
+     * Returns a string representation of selected metrics.
+     *
+     * @return A string representing selected metrics.
+     */
     public String SomeMetricsToString(){
         return"Products sold: " + productsSold + "\n" +
                 "Average note received: "  + averageNoteReceived + "\n";
     }
-
+    /**
+     * Returns a string representation of all metrics.
+     *
+     * @return A string representing all metrics.
+     */
     public String AlltoString() {
         return "Since inception," + "\n" +
             "Income: " + revenue + "$\n" +
